c7e938121653a6595798123a1bb46e1df7f94709
Kayrxn <xharukax99@gmail.com>
2026-02-12 10:13:35 +0100

    test(test_piedra_papel_tijera.py): Añadir LAGARTO y SPOCK a los casos test

--- test/test_piedra_papel_tijera.py
+++ test/test_piedra_papel_tijera.py
@@ -1,118 +1,88 @@
 import pytest
-from enum import Enum
-from src.piedra_papel_tijeras import (
-    PIEDRA, PAPEL, TIJERAS, LAGARTO, SPOCK,
-    mapa_ganador
-)
+from src.piedra_papel_tijeras import (PIEDRA, PAPEL, TIJERAS, LAGARTO, SPOCK, JuegoPiedraPapelTijeras)
 
-
-class AccionJuego(Enum):
-    """Mapeo de acciones del juego"""
-    Piedra = PIEDRA
-    Papel = PAPEL
-    Tijeras = TIJERAS
-    Lagarto = LAGARTO
-    Spock = SPOCK
-
-
-class ResultadoJuego(Enum):
-    """Resultados posibles del juego"""
-    Empate = 'empate'
-    Victoria = 'victoria'
-    Derrota = 'derrota'
-
-
-def evaluar_juego(accion_usuario, accion_computadora):
-    """Evalúa el resultado del juego entre dos acciones AccionJuego.
-    
-    Returns:
-        ResultadoJuego: Empate, Victoria o Derrota
-    """
-    usuario_val = accion_usuario.value
-    computadora_val = accion_computadora.value
-    
-    if usuario_val == computadora_val:
-        return ResultadoJuego.Empate
-    
-    victorias_usuario = mapa_ganador.get(usuario_val, {})
-    if computadora_val in victorias_usuario:
-        return ResultadoJuego.Victoria
-    
-    victorias_computadora = mapa_ganador.get(computadora_val, {})
-    if usuario_val in victorias_computadora:
-        return ResultadoJuego.Derrota
-    
-    return ResultadoJuego.Empate
-
+@pytest.fixture
+def juego():
+    return JuegoPiedraPapelTijeras()
 
 @pytest.mark.empate
-def test_empate():
-    '''
-    Partidas con empate
-    '''
+def test_empate(juego, capsys):
+    for opcion in [PIEDRA, PAPEL, TIJERAS, LAGARTO, SPOCK]:
+        juego.evaluar_juego(opcion, opcion)
+        out = capsys.readouterr().out
+        assert 'Empate' in out
 
-    assert ResultadoJuego.Empate == evaluar_juego(
-        accion_usuario=AccionJuego.Piedra,
-        accion_computadora=AccionJuego.Piedra)
+@pytest.mark.spock
+def test_spock(juego, capsys):
+    juego.evaluar_juego(SPOCK, PAPEL)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(SPOCK, LAGARTO)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(SPOCK, TIJERAS)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
+    juego.evaluar_juego(SPOCK, PIEDRA)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
 
-    assert ResultadoJuego.Empate == evaluar_juego(
-        accion_usuario=AccionJuego.Tijeras, 
-        accion_computadora=AccionJuego.Tijeras)
-
-    assert ResultadoJuego.Empate == evaluar_juego(
-        accion_usuario=AccionJuego.Papel,
-        accion_computadora=AccionJuego.Papel)
+@pytest.mark.lagarto
+def test_lagarto(juego, capsys):
+    juego.evaluar_juego(LAGARTO, PIEDRA)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(LAGARTO, TIJERAS)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(LAGARTO, SPOCK)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
+    juego.evaluar_juego(LAGARTO, PAPEL)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
 
 @pytest.mark.piedra
-def test_piedra_pierde():
-    '''
-    Piedra pierde con Papel 
-    '''
-    assert ResultadoJuego.Victoria == evaluar_juego(
-        accion_usuario=AccionJuego.Papel,
-        accion_computadora=AccionJuego.Piedra)
-
-@pytest.mark.piedra
-def test_piedra_gana():
-    '''
-    Piedra gana a Tijeras
-    '''
-    assert ResultadoJuego.Derrota == evaluar_juego(
-        accion_usuario=AccionJuego.Tijeras,
-        accion_computadora=AccionJuego.Piedra)
+def test_piedra(juego, capsys):
+    juego.evaluar_juego(PIEDRA, PAPEL)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(PIEDRA, SPOCK)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(PIEDRA, TIJERAS)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
+    juego.evaluar_juego(PIEDRA, LAGARTO)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
 
 @pytest.mark.papel
-def test_papel_pierde():
-    '''
-    Papel pierde con Tijeras
-    '''
-    assert ResultadoJuego.Victoria == evaluar_juego(
-        accion_usuario=AccionJuego.Tijeras,
-        accion_computadora=AccionJuego.Papel)
-
-@pytest.mark.papel
-def test_papel_gana():
-    '''
-    Papel gana a Piedra
-    '''
-    assert ResultadoJuego.Derrota == evaluar_juego(
-        accion_usuario=AccionJuego.Piedra,
-        accion_computadora=AccionJuego.Papel)
+def test_papel(juego, capsys):
+    juego.evaluar_juego(PAPEL, TIJERAS)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(PAPEL, LAGARTO)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(PAPEL, PIEDRA)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
+    juego.evaluar_juego(PAPEL, SPOCK)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
 
 @pytest.mark.tijeras
-def test_tijeras_pierde():
-    '''
-    Tijeras pierde con Piedra 
-    '''
-    assert ResultadoJuego.Victoria == evaluar_juego(
-        accion_usuario=AccionJuego.Piedra,
-        accion_computadora=AccionJuego.Tijeras)
-
-@pytest.mark.tijeras
-def test_tijeras_gana():
-    '''
-    Tijeras gana a Papel 
-    '''
-    assert ResultadoJuego.Derrota == evaluar_juego(
-        accion_usuario=AccionJuego.Papel,
-        accion_computadora=AccionJuego.Tijeras)
+def test_tijeras(juego, capsys):
+    juego.evaluar_juego(TIJERAS, SPOCK)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(TIJERAS, PIEDRA)
+    out = capsys.readouterr().out
+    assert 'Has perdido' in out or 'Has ganado' in out
+    juego.evaluar_juego(TIJERAS, LAGARTO)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
+    juego.evaluar_juego(TIJERAS, PAPEL)
+    out = capsys.readouterr().out
+    assert 'Has ganado' in out or 'Has perdido' in out
